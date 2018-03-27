01004eae3806a4e36371d3db122147da0d741ba8
Sese-khalid <Sese-khalid@users.noreply.github.com>
2018-03-27 14:31:31 -0400

    update

---
+++ sliderscript.js
@@ -0,0 +1,38 @@
+﻿
+
+$(“.slidercontent”).hide();
+
+$(“.slidercontent:nth-child(1)”).fadeIn(“slow”); 
+
+var current = 1; var max = $( “li” ).length +1; 
+
+function changeUp() { 
+
+	$(“.slidercontent”).hide(); 
+	current += 1; 
+
+   if (current === max) { 
+       current = 1; 
+	} 
+	$(“.slidercontent:nth-child(“ + current + “)”).fadeIn(“slow”); }
+
+function changeDown() { 
+	$(“.slidercontent”).hide(); 
+	current -= 1;
+    if (current === 0) { 
+	current = max — 1; 
+   	}
+
+$(“.slidercontent:nth-child(“ + current + “)”).fadeIn(“slow”); } 
+
+$(“.sliderspot2”).click(function() { changeUp(); });
+
+$(“.sliderspot”).click(function() { changeDown(); }); 
+
+function startChange() { changeIt = setInterval(function(){ changeUp(); }, 10000); }; 
+
+function stopChange() { clearInterval(changeIt); } 
+$(“.sliderspot2”).click(function() { stopChange(); changeUp(); startChange(); });
+
+$(“.sliderspot”).click(function() { stopChange(); changeDown(); startChange(); }); 
+
